22dd198f8ee4660bfd07c8bc430764e93a67fe16
Allan Farca <allan.farca@gmail.com>
2020-05-23 05:41:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, SUM(characters.species)
+  "SELECT series.title
    FROM series
    INNER JOIN authors
    ON series.author_id = authors.id
