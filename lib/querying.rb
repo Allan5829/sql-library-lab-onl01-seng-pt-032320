7da9bd7ebb780b66fd96887f290c3b5c675ecb0d
Allan Farca <allan.farca@gmail.com>
2020-05-23 05:56:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
    FROM authors
    JOIN series
    ON authors.id = series.author_id
-   JOIN subgenres
+   INNER JOIN subgenres
    ON series.subgenre_id = subgenres.id;"
 end
 
