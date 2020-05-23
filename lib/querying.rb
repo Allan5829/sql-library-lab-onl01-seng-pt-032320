bb823cb3b1185fa0f05b5cb83bfdb038ea561a20
Allan Farca <allan.farca@gmail.com>
2020-05-23 05:22:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.species, COUNT(character_books.book_id) AS appearences
    FROM characters
    INNER JOIN character_books
-   ON characters.if = character_books.character_id
+   ON characters.id = character_books.character_id
    GROUP BY (characters.names)
    ORDER BY appearences DESC;"
 end
