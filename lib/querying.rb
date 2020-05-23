4a6593eefb641b94bd276bb42f67e8d653bc60aa
Allan Farca <allan.farca@gmail.com>
2020-05-23 05:45:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
    ON series.author_id = authors.id
    JOIN characters
    ON authors.id = characters.author_id
-   GROUP BY series.title HAVING characters.species = 'human';"
+   GROUP BY series.title HAVING characters.species = 'human'
+   ORDER BY SUM (characters.species) DESC;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
