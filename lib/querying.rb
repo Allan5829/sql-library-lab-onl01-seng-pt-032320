6b23e83ddd327bc896f4d15d8ddce6b51dbc7be0
Allan Farca <allan.farca@gmail.com>
2020-05-23 04:53:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT Count(characters.species), characters.species
+   FROM characters
+   ORDER BY characters.motto ASC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
