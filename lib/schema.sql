c42edec0e369e469e270e0bcd7cacdb3c8e8b6ec
Allan Farca <allan.farca@gmail.com>
2020-05-23 04:15:04 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sub-genres
+    (id INTEGER PRIMARY KEY,
+  );
+
+CREATE TABLE series
+    (id INTEGER PRIMARY KEY,
+    title TEXT,
+    author_id TEXT,
+    sub-genre_id TEXT
+  );
+
+CREATE TABLE series
+    (id INTEGER PRIMARY KEY,
+  );
